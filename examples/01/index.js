504a8504f198481ce8b602f176ac012647abe8e2
Cole Furfaro-Strode <colestrode@gmail.com>
2016-11-15 22:25:32 -0500

    initial commit

---
+++ examples/01/index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const request = require('request')
+const Ws = require('ws')
+let lastMessageId = 0
+let rtm
+
+// use Slack web API to get an RTM API URL
+request({
+  method: 'GET',
+  uri: `https://slack.com/api/rtm.start`,
+  json: true,
+  qs: {
+    no_unreads: true,
+    simple_latest: true,
+    token: process.env.SLACK_TOKEN
+  }
+}, rtmStart)
+
+
+function rtmStart (err, res, body) {
+  // handle some errors
+  if (err || res.statusCode >= 400 || !body.ok) {
+    return console.log('Error starting RTM', err || body)
+  }
+
+  // use the url to open a new websocket
+  rtm = new Ws(body.url, null, {agent: null})
+
+  // hey look! it worked!
+  rtm.on('open', () => {
+    console.log(`connected to ${body.team.name} (${body.team.id}) as ${body.self.name} (${body.self.id})`)
+  })
+
+  // let's just bail if something bad happens, you could try to reconnect
+  rtm.on('error', (err) => {
+    console.log('oh man, an error happened!', err)
+    process.exit(1)
+  })
+
+
+  // this is where the magic will happen
+  rtm.on('message', (event) => {
+    const message = JSON.parse(event)
+    // slack has many message types: https://api.slack.com/rtm
+    if (message.type === 'message') {
+      messageHandler(message)
+    }
+  })
+}
+
+// this callback decides how we respond to a "message" event
+function messageHandler (message) {
+  if (/hello/.test(message.text)) {
+    reply(message, 'GO CUBSSSSS')
+  } else if (/howdy/.test(message.text)) {
+    reply(message, 'GO TRIBE')
+  }
+}
+
+// reply using the websocket connection
+function reply (message, text) {
+  const replyMessage = {
+    id: ++lastMessageId, // need a unique (for this connection), non-negative ID for this message
+    type: 'message',
+    channel: message.channel,
+    text: text
+  }
+
+  rtm.send(JSON.stringify(replyMessage), (err) => {
+    if (err) {
+      return console.log(`could not reply to message ${message.ts} in ${message.channel}`)
+    }
+  })
+}
